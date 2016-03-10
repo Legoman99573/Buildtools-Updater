e1136de5d048fe7c73d3b7a63018b5a591b94ee3
Legoman99573 <christianwhead@gmail.com>
2016-03-09 18:09:54 -0600

    Create run.bat

---
+++ files/buildtools/run.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set startdir=%~dp0
+
+set content=
+for /f "delims=" %%i in ('type %startdir%\..\..\config\gitlocation.txt') do set content= %%i
+
+set version=
+for /f "delims=" %%i in ('type %startdir%\..\..\config\version.txt') do set version= %%i
+
+%content% --login -i -c "java -jar ""%startdir%\..\..\files\buildtools\BuildTools.jar"" -rev %version% " -o %startdir%\..\..\files\buildtools\BuildTools.jar "
+
+pause
