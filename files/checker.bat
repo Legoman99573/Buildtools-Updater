c6aa30f28770ef2aef7ba571bad5520247ed0067
Legoman99573 <christianwhead@gmail.com>
2016-03-09 18:19:54 -0600

    made additions to get this to work

--- files/checker.bat
+++ files/checker.bat
@@ -1,25 +1,35 @@
 @echo off
 
-set startdir=%~dp0
-
-if exist \dependency (goto step2) else (md dependency)
+if exist %startdir%\files\buildtools (goto step2) else (md files\buildtools
+echo Buildtools will be stored here  > files\buildtools\info.txt
+goto step2)
 
 :step2
-if exist \api (goto step3) else (md api)
+if exist %startdir%\api (goto step3) else (md api
+echo API's will be stored here  > api\info.txt
+goto step3)
 
 :step3
-if exist \config (goto config) else (md config
+if exist %startdir%\config (goto config) else (md config
 echo 1.9  > config\version.txt
-echo created version.txt)
+echo C:\Program Files\Git\bin\bash.exe  > config\gitlocation.txt
+goto config)
 
 :config
-if exist config\version.txt (goto config2) else (echo 1.9  > config\version.txt
-echo created version.txt
-goto config2)
-:config2
-if exist config\gitlocation.txt (goto last) else (echo C:\Program Files\Git\bin\bash.exe  > config\gitlocation.txt
-echo created gitlocation.txt
-goto last)
 
-:last
+if Exist %startdir%\files\menu.bat (goto next) else (%content% --login -i -c "curl -o menu.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/menu.bat"
+move menu.bat %startdir%\files)
+goto next
+
+:next
+if Exist %startdir%\files\buildtools\delbt.bat (goto next2) else (%content% --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
+move delbt.bat %startdir%\files\bt)
+goto next2
+
+:next2
+if Exist %startdir%\files\buildtools\run.bat (goto exit) else (%content% --login -i -c "curl -o run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/run.bat"
+move run.bat %startdir%\files\bt)
+
+
+:exit
 exit
