f83f5332bae316e5e7e57f64024d8c2b4076adcc
Legoman99573 <christianwhead@gmail.com>
2016-03-11 00:06:36 -0600

    Fixed crash due to files directory not generating.

--- BuildTools.bat
+++ BuildTools.bat
@@ -15,7 +15,8 @@ If /i "%_1%"=="N" goto stop
 If /i "%_1%"=="n" goto stop
 
 :start
-if Exist %startdir%\files\checker.bat (goto next) else (%content% --login -i -c "curl -o checker.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/checker.bat"
+if Exist %startdir%\files\checker.bat (goto next) else (md %startdir%\files
+%content% --login -i -c "curl -o checker.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/checker.bat"
 move checker.bat %startdir%\files)
 goto next
 
