0cd3333cd35a8ee53ec4f47e3927d14bc4119a6e
Legoman99573 <christianwhead@gmail.com>
2016-03-11 15:16:46 -0600

    Update checker.bat

--- files/checker.bat
+++ files/checker.bat
@@ -1,7 +1,7 @@
 @echo off
 
 %content% --login -i -c "curl -o menu.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/menu.bat"
-move menu.bat %startdir%\files)
+move menu.bat %startdir%\files
 
 %content% --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
 move delbt.bat %startdir%\files\buildtools
