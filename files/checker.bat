d3a779d942d76295537efc8257414ec12ada00fd
Legoman99573 <christianwhead@gmail.com>
2016-03-11 15:12:27 -0600

    Setup was added to get it ready due to breaking in checker.bat
    checker.bat will be used as an updater ;)

--- files/checker.bat
+++ files/checker.bat
@@ -1,35 +1,57 @@
 @echo off
 
+set startdir=%~dp0
+set v=0.8 Beta
+set content=
+for /f "delims=" %%i in ('type config\gitlocation.txt') do set content= %%i
+
+:boot
+@echo This build is a beta, and could break important files. Continue: 
+Set /P _1=(Y, N) || Set _1=NothingChosen
+If "%_1%"=="NothingChosen" goto :setup
+If /i "%_1%"=="Y" goto setup
+If /i "%_1%"=="y" goto setup
+If /i "%_1%"=="N" goto stop
+If /i "%_1%"=="n" goto stop
+
+:setup
+cls
+if exist %startdir%\config (goto launch) else (md config
+echo 1.9  > config\version.txt
+echo C:\Program Files\Git\bin\bash.exe  > config\gitlocation.txt
+goto launch)
+
+:launch
 if exist %startdir%\files\buildtools (goto step2) else (md files\buildtools
 echo Buildtools will be stored here  > files\buildtools\info.txt
 goto step2)
 
 :step2
-if exist %startdir%\api (goto step3) else (md api
+if exist %startdir%\api (goto start) else (md api
 echo API's will be stored here  > api\info.txt
-goto step3)
+goto start)
 
-:step3
-if exist %startdir%\config (goto config) else (md config
-echo 1.9  > config\version.txt
-echo C:\Program Files\Git\bin\bash.exe  > config\gitlocation.txt
-goto config)
 
-:config
-
-if Exist %startdir%\files\menu.bat (goto next) else (%content% --login -i -c "curl -o menu.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/menu.bat"
-move menu.bat %startdir%\files)
+:start
+cls
+if Exist %startdir%\files\checker.bat (goto next) else (md %startdir%\files
+%content% --login -i -c "curl -o checker.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/checker.bat"
+move checker.bat %startdir%\files)
 goto next
 
 :next
-if Exist %startdir%\files\buildtools\delbt.bat (goto next2) else (%content% --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
-move delbt.bat %startdir%\files\buildtools)
+start "Buildtools Updater v.%v% | Checker" /wait %startdir%\files\checker.bat
 goto next2
 
 :next2
-if Exist %startdir%\files\buildtools\run.bat (goto exit) else (%content% --login -i -c "curl -o run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/run.bat"
-move run.bat %startdir%\files\buildtools)
+if Exist %startdir%\files\menu.bat (goto ready) else (md %startdir%\files
+%content% --login -i -c "curl -o menu.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/menu.bat"
+move menu.bat %startdir%\files)
+goto ready
 
+:ready
+start "Buildtools Updater v.%v%" /Max /i %startdir%\files\menu.bat
+goto exit
 
 :exit
 exit
