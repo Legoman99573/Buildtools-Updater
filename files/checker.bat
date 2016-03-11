942678f393f61d1da67f7a96cc02c673877db059
Legoman99573 <christianwhead@gmail.com>
2016-03-11 15:28:46 -0600

    Update checker.bat

--- files/checker.bat
+++ files/checker.bat
@@ -1,12 +1,40 @@
 @echo off
 
-%content% --login -i -c "curl -o menu.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/menu.bat"
-move menu.bat %startdir%\files
+set startdir=%~dp0
+set content=
+for /f "delims=" %%i in ('type config\gitlocation.txt') do set content= %%i
 
-%content% --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
-move delbt.bat %startdir%\files\buildtools
+if exist %startdir%\files\buildtools (goto step2) else (md files\buildtools
+echo Buildtools will be stored here  > files\buildtools\info.txt
+goto step2)
 
-%content% --login -i -c "curl -o run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/run.bat"
-move run.bat %startdir%\files\buildtools
+:step2
+if exist %startdir%\api (goto step3) else (md api
+echo API's will be stored here  > api\info.txt
+goto step3)
 
+:step3
+if exist %startdir%\config (goto config) else (md config
+echo 1.9  > config\version.txt
+echo C:\Program Files\Git\bin\bash.exe  > config\gitlocation.txt
+goto config)
+
+:config
+
+if Exist %startdir%\files\menu.bat (goto next) else (%content% --login -i -c "curl -o menu.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/menu.bat"
+move menu.bat %startdir%\files)
+goto next
+
+:next
+if Exist %startdir%\files\buildtools\delbt.bat (goto next2) else (%content% --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
+move delbt.bat %startdir%\files\buildtools)
+goto next2
+
+:next2
+if Exist %startdir%\files\buildtools\run.bat (goto exit) else (%content% --login -i -c "curl -o run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/run.bat"
+move run.bat %startdir%\files\buildtools)
+
+
+:exit
 exit
+
