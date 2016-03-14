b43c75409c205b57d47d210b2c2c488ae70fce71
Legoman99573 <christianwhead@gmail.com>
2016-03-14 10:46:49 -0500

    Plugin-fixer added
    Changes:
    • plugin-fixer will launch if you use command "plugin"
    • When error-1 occurs, your browser will launch
    • help command will be added later. Sorry if you get an error.

--- files/menu.bat
+++ files/menu.bat
@@ -1,7 +1,8 @@
 @echo off
 
 set startdir=%~dp0
-set v=0.8 Beta
+set v=
+for /f "delims=" %%i in ('type files\btversion.txt') do set v= %%i
 
 set content=
 for /f "delims=" %%i in ('type config\gitlocation.txt') do set content= %%i
@@ -15,15 +16,18 @@ If "%_1%"=="NothingChosen" goto :start
 If /i "%_1%"=="update" goto update
 If /i "%_1%"=="run" goto run
 If /i "%_1%"=="help" goto help
+If /i "%_1%"=="plugin" goto plugin
 If /i "%_1%"=="exit" goto exit
 
 :update
+cls
 start "Buildtools Updater v.%v% | Delete Buildtools.jar" /b %startdir%buildtools\delbt.bat
 %content% --login -i -c "curl -o files/buildtools/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastBuild/artifact/target/BuildTools.jar"
 if exist %startdir%buildtools\BuildTools.jar (@echo Updated BuildTools) else (@echo An error has occured. Make sure folder has write access)
 goto start
 
 :run
+cls
 @echo getting Buildtools Ready
 if exist %startdir%\buildtools\apache-maven-3.2.5 (move %startdir%\buildtools\apache-maven-3.2.5 %startdir%..\) else (echo Folder "apache-maven-3.2.5" doesnt exist may be ignored)
 if exist %startdir%\buildtools\BuildData (move %startdir%\buildtools\BuildData %startdir%..\) else (echo Folder "BuildData" doesnt exist may be ignored)
@@ -47,16 +51,25 @@ move %startdir%..\work %startdir%\buildtools\
 goto start
 
 :help
-start "Buildtools Updater v.%v% | Buildtools Help" help.bat
+cls
+start "Buildtools Updater v.%v% | Buildtools Help" /b ..\files\help-%v%.bat
+goto start
+
+:plugin
+cls
+start "Buildtools Updater v.%v% | Buildtools Help" /wait ..\files\plugin-%v%.bat
 goto start
 
 :error1
+cls
 @echo Error-1 has occured. Goto https://www.spigotmc.org/wiki/buildtools-updater/ to find this error
 if exist %startdir%\error (goto errorprint1) else (md error)
 :errorprint1
 echo Link to Git: https://github.com/git-for-windows/git/releases/download/v2.7.2.windows.1/Git-2.7.2-64-bit.exe  > error\error-1.txt
+cmd /c start https://github.com/git-for-windows/git/releases/download/v2.7.2.windows.1/Git-2.7.2-64-bit.exe
 
 :exit
+cls
 @echo Thanks for using BuildTools Updater v.%v% by: Legoman99573
 @pause
 exit
