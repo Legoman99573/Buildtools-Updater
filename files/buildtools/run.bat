00338d58f28df3f55623e8f4802846c07afcac0a
Legoman99573 <christianwhead@gmail.com>
2016-03-16 11:18:44 -0500

    Fixed C:\Program not a command error

--- files/buildtools/run.bat
+++ files/buildtools/run.bat
@@ -8,6 +8,6 @@ for /f "delims=" %%i in ('type %startdir%\..\..\config\gitlocation.txt') do set 
 set version=
 for /f "delims=" %%i in ('type %startdir%\..\..\config\version.txt') do set version= %%i
 
-%content% --login -i -c "java -jar ""%startdir%\..\..\files\buildtools\BuildTools.jar"" -rev %version% " -o %startdir%\..\..\files\buildtools\BuildTools.jar "
+"%content%" --login -i -c "java -jar ""%startdir%\..\..\files\buildtools\BuildTools.jar"" -rev %version% " -o %startdir%\..\..\files\buildtools\BuildTools.jar "
 
 pause
