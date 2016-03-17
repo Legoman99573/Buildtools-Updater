565c0d75d30ae13fc3139776a20470a11065c524
Legoman99573 <christianwhead@gmail.com>
2016-03-17 12:36:03 -0500

    Fixed where it wouldnt exit

--- files/buildtools/run.bat
+++ files/buildtools/run.bat
@@ -3,11 +3,11 @@
 set startdir=%~dp0
 
 set content=
-for /f "delims=" %%i in ('type %startdir%\..\..\config\gitlocation.txt') do set content= %%i
+for /f "delims=" %%i in ('type %startdir%\..\..\config\gitlocation.txt') do set content=%%i
 
 set version=
-for /f "delims=" %%i in ('type %startdir%\..\..\config\version.txt') do set version= %%i
+for /f "delims=" %%i in ('type %startdir%\..\..\config\version.txt') do set version=%%i
 
 "%content%" --login -i -c "java -jar ""%startdir%\..\..\files\buildtools\BuildTools.jar"" -rev %version% " -o %startdir%\..\..\files\buildtools\BuildTools.jar "
 
-pause
+exit
