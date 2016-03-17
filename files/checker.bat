1e5462283fc7fd1a87aa4fabee57489f4b819f97
Legoman99573 <christianwhead@gmail.com>
2016-03-17 16:34:08 -0600

    fixed updater causing errors

--- files/checker.bat
+++ files/checker.bat
@@ -16,27 +16,18 @@ if exist %startdir%\api (goto step3) else (md api
 echo API's will be stored here  > api\info.txt
 goto step3)
 
-:step3
-if exist %startdir%\config (goto config) else (md config
-echo 1.9  > config\version.txt
-echo C:\Program Files\Git\bin\bash.exe  > config\gitlocation.txt
-goto config)
-
 :config
 
-if Exist %startdir%\files\buildtools\delbt.bat (goto next2) else (%content% --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
-move delbt.bat %startdir%\files\buildtools)
+if Exist %startdir%\files\buildtools\delbt.bat (goto next2) else (%content% --login -i -c "curl -o files/buildtools/delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/0.10.4-Beta/files/buildtools/delbt.bat ")
 goto next2
 
 :next2
-if Exist %startdir%\files\buildtools\run.bat (goto next3) else (%content% --login -i -c "curl -o run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/%v%/files/buildtools/run.bat"
-move run.bat %startdir%\files\buildtools)
+if Exist %startdir%\files\buildtools\run.bat (goto next3) else (%content% --login -i -c "curl -o files/buildtools/run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/0.10.4-Beta/files/buildtools/run.bat ")
 goto next3
 
 :next3
-if Exist %startdir%\files\plugin-%v%.bat (goto exit) else (%content% --login -i -c "curl -o plugin-%v%.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/%v%/files/plugin.bat"
-move plugin-%v%.bat %startdir%\files\buildtools)
-
+if Exist %startdir%\files\plugin-%v%.bat (goto exit) else (%content% --login -i -c "curl -o files/buildtools/plugin-%v%.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/0.10.4-Beta/files/plugin.bat ")
+goto exit
 
 :exit
 exit
