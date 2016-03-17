3e4adb048bd3069e4cb2f455d9aaf415c2cf4236
Legoman99573 <christianwhead@gmail.com>
2016-03-17 12:15:03 -0500

    Fixed ser variables overlapping

--- files/checker.bat
+++ files/checker.bat
@@ -2,9 +2,10 @@
 
 set startdir=%~dp0
 set v=
-for /f "delims=" %%i in ('type files\btversion.txt') do set content= %%i
+for /f "delims=" %%i in ('type files\btversion.txt') do set content=%%i
+
 set content=
-for /f "delims=" %%i in ('type config\gitlocation.txt') do set content= %%i
+for /f "delims=" %%i in ('type config\gitlocation.txt') do set content=%%i
 
 if exist %startdir%\files\buildtools (goto step2) else (md files\buildtools
 echo Buildtools will be stored here  > files\buildtools\info.txt
@@ -23,17 +24,17 @@ goto config)
 
 :config
 
-if Exist %startdir%\files\buildtools\delbt.bat (goto next2) else ("%content%" --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
+if Exist %startdir%\files\buildtools\delbt.bat (goto next2) else (%content% --login -i -c "curl -o delbt.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/master/files/buildtools/delbt.bat"
 move delbt.bat %startdir%\files\buildtools)
 goto next2
 
 :next2
-if Exist %startdir%\files\buildtools\run.bat (goto next3) else ("%content%" --login -i -c "curl -o run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/%v%/files/buildtools/run.bat"
+if Exist %startdir%\files\buildtools\run.bat (goto next3) else (%content% --login -i -c "curl -o run.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/%v%/files/buildtools/run.bat"
 move run.bat %startdir%\files\buildtools)
 goto next3
 
 :next3
-if Exist %startdir%\files\plugin-%v%.bat (goto exit) else ("%content%" --login -i -c "curl -o plugin-%v%.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/%v%/files/plugin.bat"
+if Exist %startdir%\files\plugin-%v%.bat (goto exit) else (%content% --login -i -c "curl -o plugin-%v%.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/%v%/files/plugin.bat"
 move plugin-%v%.bat %startdir%\files\buildtools)
 
 
