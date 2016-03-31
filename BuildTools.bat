27216c69817fde77e0330fa479d418fe96f01a64
Legoman99573 <christianwhead@gmail.com>
2016-03-31 17:29:56 -0600

    fixes :|

--- BuildTools.bat
+++ BuildTools.bat
@@ -2,11 +2,28 @@
 
 set startdir=%~dp0
 
-set v=
-for /f "delims=" %%i in ('type files\btversion.txt') do set v=%%i
-
 set content=
 for /f "delims=" %%i in ('type config\gitlocation.txt') do set content=%%i
+
+if exist %startdir%tasks (goto step2) else (goto exit)
+
+:step2
+if exist %startdir%api (goto step3) else (goto exit)
+
+:step3
+if exist %startdir%config (goto config) else (goto exit)
+
+:config
+
+if Exist tasks/delbt.bat (goto next2) else (goto exit)
+goto next2
+
+:next2
+if Exist tasks/run.bat (goto next3) else (goto exit)
+goto next3
+
+:next3
+if Exist tasks/pluginfixer.bat (goto exit) else (goto exit)
 
 :boot
 If exist %content% (goto boot2) else (@echo bash.exe was not found. Download, or configure gitlocation.txt
@@ -20,18 +37,12 @@ If /i "%_1%"=="y" goto start
 If /i "%_1%"=="N" goto stop
 If /i "%_1%"=="n" goto stop
 
-start "Buildtools Updater v.%v% | Checker" /wait %startdir%files\checker-%v%.bat
-goto next2
-
-:next2
-if Exist %startdir%files\menu-%v%.bat (goto ready) else (if exist %startdir%\files\menu-*.bat (del /f files\menu-*.bat) else (@echo couldnt find an old version of menu.bat. This can be ignored.
-if exist %startdir%files (@echo files directory was found) else (@echo files directory was not found. Generating directory
-md %startdir%files)
-%content% --login -i -c "curl -o files/menu-%v%.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/0.10.4-Beta/files/menu.bat ")
+:start
+if Exist menu.bat (goto ready) else (@echo An error has occured. Redownload this script.)
 goto ready
 
 :ready
-start "Buildtools Updater v.%v%" /Max /i %startdir%files\menu-%v%.bat
+start "Buildtools Updater v.0.14-Beta" /Max /i %startdir%files\menu-%v%.bat
 goto exit
 
 :exit
