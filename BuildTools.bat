1e76e3f82e8d8e0dfad19383e2505d2e6b00c1af
Legoman99573 <christianwhead@gmail.com>
2016-03-31 11:46:34 -0600

    Recoded

--- BuildTools.bat
+++ BuildTools.bat
@@ -1,30 +1,12 @@
 @echo off
 
 set startdir=%~dp0
-
-set v=
-for /f "delims=" %%i in ('type files\btversion.txt') do set v=%%i
-
+set v=0.14 Beta
 set content=
-for /f "delims=" %%i in ('type config\gitlocation.txt') do set content=%%i
-
-if exist %startdir%\config\gitlocation.txt (goto boot) else (md config
-C:\Program Files\Git\bin\bash.exe --login -i -c "curl -o config/gitlocation.txt http://thegearmc.com/update/gitlocation.txt"
-C:\Program Files\Git\bin\bash.exe --login -i -c "curl -o config/version.txt http://thegearmc.com/update/version.txt"
-C:\Program Files\Git\bin\bash.exe --login -i -c "curl -o config/plugin.txt http://thegearmc.com/update/plugin.txt"
-)
-
-If exist %startdir%files\checker.bat (del /f files\checker.bat) else (goto menu)
-:menu
-If exist %startdir%files\menu.bat (del /f files\menu.bat) else (goto plugin)
-:plugin
-If exist %startdir%files\plugin.bat (del /f files\plugin.bat) else (goto boot)
+for /f "delims=" %%i in ('type config\gitlocation.txt') do set content= %%i
 
 :boot
-If exist %content% (goto boot2) else (@echo bash.exe was not found. Download, or configure gitlocation.txt
-Goto error)
-:boot2
-@echo This build is in beta and could break important files. Continue: 
+@echo This build is a beta, and could break important files. Continue: 
 Set /P _1=(Y, N) || Set _1=NothingChosen
 If "%_1%"=="NothingChosen" goto :start
 If /i "%_1%"=="Y" goto start
@@ -33,32 +15,21 @@ If /i "%_1%"=="N" goto stop
 If /i "%_1%"=="n" goto stop
 
 :start
-If exist %startdir%files (@echo Directory already exists) else (md %startdir%files)
-if exist files\btversion.txt (del /f files\btversion.txt) else (@echo btversion was not found. This can be ignored)
-%content% --login -i -c "curl -o files/btversion.txt http://thegearmc.com/update/btversion.txt "
-
-if exist %startdir%files\checker-%v%.bat (goto next) else (if exist %startdir%files\checker-%v%.bat (del /f files\checker-*.bat) else (@Echo Couldnt find old version of checker.bat. This can be Ignored)
-%content% --login -i -c "curl -o files/checker-%v%.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/0.10.4-Beta/files/checker.bat ")
+if Exist checker.bat (goto next) else (@echo Missing file checker.bat. Please redownload this script.
+goto exit)
 goto next
 
 :next
-start "Buildtools Updater v.%v% | Checker" /wait %startdir%files\checker-%v%.bat
-goto next2
-
-:next2
-if Exist %startdir%files\menu-%v%.bat (goto ready) else (if exist %startdir%\files\menu-*.bat (del /f files\menu-*.bat) else (@echo couldnt find an old version of menu.bat. This can be ignored.
-if exist %startdir%files (@echo files directory was found) else (@echo files directory was not found. Generating directory
-md %startdir%files)
-%content% --login -i -c "curl -o files/menu-%v%.bat https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater/0.10.4-Beta/files/menu.bat ")
+start "Checker" checker.bat
 goto ready
 
 :ready
-start "Buildtools Updater v.%v%" /Max /i %startdir%files\menu-%v%.bat
+if Exist menu.bat (goto next2) else (@echo Missing file menu.bat. Please redownload this script.
+goto exit)
+:next2
+start "Buildtools Updater v.%v%" /Max /i %startdir%menu.bat
 goto exit
 
 :exit
+@pause
 exit
-
-:error
-@echo Current invalid location set: %content%
-pause
