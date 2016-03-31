eb0aa4b47d999f79772c0c81804581165be1271a
Legoman99573 <christianwhead@gmail.com>
2016-03-31 17:27:11 -0600

    Update BuildTools.bat

--- BuildTools.bat
+++ BuildTools.bat
@@ -8,26 +8,6 @@ for /f "delims=" %%i in ('type files\btversion.txt') do set v=%%i
 set content=
 for /f "delims=" %%i in ('type config\gitlocation.txt') do set content=%%i
 
-if exist %startdir%buildtools (goto step2) else (goto exit)
-
-:step2
-if exist %startdir%api (goto step3) else (goto exit)
-
-:step3
-if exist %startdir%config (goto config) else (goto exit)
-
-:config
-
-if Exist tasks/delbt.bat (goto next2) else (goto exit)
-goto next2
-
-:next2
-if Exist tasks/run.bat (goto next3) else (goto exit)
-goto next3
-
-:next3
-if Exist tasks/pluginfixer.bat (goto exit) else (goto exit)
-
 :boot
 If exist %content% (goto boot2) else (@echo bash.exe was not found. Download, or configure gitlocation.txt
 Goto error)
